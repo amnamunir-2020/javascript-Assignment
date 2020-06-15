bfe0c0422c11d2793e93f42745c5ed05f51e83c9
amnamunir-2020 <59399151+amnamunir-2020@users.noreply.github.com>
2020-06-15 22:17:37 +0500

    15-june-2020 AllChapters
    all task in this app

---
+++ app.js
@@ -0,0 +1,541 @@
+//   CHAPTER:1==================================================================
+//Q1
+//window.alert("JAVASCRIPT ALERT\n\nError please ! Enter a valid  Password")
+//Q2
+//alert("Error please ! Enter a valid  Password")
+
+//Q3
+// alert("Welcome to JS Land\n Happy Coding!")
+
+//Q4
+
+// alert("JavaScript Alert\n\n Welcome To JS Land")
+// alert("JavaScript Alert\n\n Happy Coding!")
+
+
+//   //Q7---Head section script
+//   <script>
+//   alert("JavaScript High Level Language")
+// </script>
+// </head>
+// //Before body section script
+// <script>
+// window.alert("Welcome To JavaScript")
+// </script>
+
+// <body>
+// <H1>CHAPTER:1</H1>
+// //Between Body section script
+// <script>
+//   alert("Asslamoalikum Warhmatullahi Wabarakatuhu!")
+// </script>
+// </body>
+
+// //After Body section script
+// <script>
+// alert("Assignment Javascript Chap1")
+// </script>
+
+//CHAPTER:2=====================================================================
+//-----Q1Declare a VARIABLE USERNAME
+//  var username;
+
+
+//-------Q2 DECLARE A FULLNAME ASSIGN VALUE
+/*var fullName = "Amna Munir";
+document.write(fullName);*/
+
+//------Q3 DECLARE MESSAGE VARIABLE AND ALERT SHOW HELLOWORLD!
+/* var message;
+   message = "Hello World!";
+   alert(message);*/
+
+
+//----Q4 BIO DATA IN ALERT
+
+/* var name = "JOHN DOE";
+ var age = "15 Years old";
+ var award = " Certificate Mobile Application Development";
+
+ alert(name)
+ alert(age)
+ alert(award)*/
+
+
+//--------Q5 ALERT USING ONE VARIABLE
+
+//alert("             PIZZA \n             PIZZ \n             PIZ  \n             PI \n             P ")
+
+
+//---Q6 CONCATENATON
+
+/*var email = "amnamunir612@gmail.com";
+alert("My Email Address is  " + email)*/
+
+//  ---Q7 variable concatenate message
+/*  var book = " A smarter way to learn JavaScript"
+  alert("I am  trying to learning from  Book " + book)*/
+
+
+//----Q8 IN JS HTML CONTENT
+//  document.write("Yah! I can write HTML content through JavaScript")
+
+//----Q9 FOLLOWING STRING IN alert box
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+
+//CHAPTER:3====================================================================
+//---Q1 DECLARE ASSIGN
+/* var age = "23";
+ alert("I am " + age + " years old")*/
+
+/*var N = prompt("Enter Your visit times")
+alert("You have visited this site " + N + " times")*/
+
+
+// var birthYear = "1997";
+// document.write("My Birth Year is " + birthYear + "<br>")
+// document.write("Data type of my  declared variable is number")
+
+
+
+// var visitor = prompt("Enter Your Visitor Names")
+// var products = prompt("Enter products")
+// var quantity = prompt("Enter Quantity")
+// document.write(visitor + "  ordered  " + products + "  on " + quantity + " clothing store ")
+
+//CHAPTER:4===================================================================
+//Q1----decalare 3 variables in one statement just declare in one line multiple variables not assign value
+// var name,age ,number;
+
+
+
+//Q2--declare 5 LEGAL variables:    //decalre 5 ILLEGAL Variables
+/*  1. firstname                       1.first name
+        2. first_name                          2.11firstname
+        3. first - name
+        4. firstName
+        5. firstname11
+    For Example:camelcase
+    userAlert myVar   userResponseTime                                       */
+
+
+//Q3----
+// document.write("<h1>Rules for numbers JS Variables</h1>")
+// var contain = prompt("Enter variables contain")
+// document.writeln("Variables names can only contain  " + contain + "  . For Example $my_1st variable")
+// var start = prompt("Enter variables starting")
+// document.writeln("<br>Variable names  must begin with a " + start + ".For Example $name,_name or name ")
+// var caseSensitive = "sensitive"
+// document.write("<br>Variables names are case  " + caseSensitive)
+// var key = "keywords"
+// document.write("<br>Variables names should not be JS  " + key)
+// var only = "letters,numbers,dollar sign and underscore"
+// document.write("<br>Variable can only contain " + only)
+
+
+// <!-- CHAPTER:5============================================================== -->
+// <!-- <h1>Shopping Cart</h1> -->
+// <!-- <h1>Marks Sheet</h1> -->
+// <!-- <h1>Currency in PKR</h1> -->
+// <!-- <h1>Age Calculator</h1> -->
+// <!-- <h1>The Geometrizer</h1> -->
+// <!-- <h1>The LifeTime Supply Calculator</h1> -->
+
+//Q1--- TWO NUMBERS ADD THEM IN VARIABLE
+/*var firstNum = prompt("Enter Your First Number")
+var secondNum = prompt("Enter Your Second Number")
+var add = (+firstNum) + (+secondNum)
+document.write("Sum of " + firstNum + " and " + secondNum + " is " + add)
+    //Q2 ---SUBTRACT MULTILPY DIVISION MODULUS
+var sub = firstNum - secondNum
+document.write("<br>Subtract this number " + firstNum + "-" + secondNum + "=" + sub)
+var mul = firstNum * secondNum
+document.write("<br>Multiply two numbers  " + firstNum + " *" + secondNum + "=" + mul)
+var div = firstNum / secondNum
+document.write("<br>Division of number " + firstNum + "/" + secondNum + " =" + div)
+var mod = firstNum % secondNum
+document.write("<br>Modulus of numbers " + firstNum + "%" + secondNum + "=" + mod)*/
+
+
+
+//Q3---
+//--declare a variable  and no value assign
+/*  var a;
+  document.write("Value after variable declaration is " + a)
+      //--some number variable
+  var year2020 = "Variable with  number declare"
+  document.write("<br>" + year2020)
+      //--initial value
+  var init = 5
+  document.write("<br>Initial  Value :" + init)
+      //--increment by 1
+  init++
+  document.write("<br>Value After Increament is:" + init)
+      //--incrment by 7
+  init += 7
+  document.write("<br>Value After Addition is:" + init)
+      //--decrement -1
+  init--
+  document.write("<br>Value After Decrement is:" + init)
+      //--current value variable divide by 3 
+      //Remainder show
+  document.write("<br>Remainder is:" + init % 3)      */
+
+
+///Q4----Cost
+/* var costofOneMovie = 600
+ var movie5 = 5 * costofOneMovie
+ document.write("<br> Total cost to buy 5 tickets to a movie is " + movie5 + "PKR")  */
+
+
+
+
+//Q5  TABLE:
+/*var table = prompt("Enter Your Number ")
+document.write("Table of " + table + "<br>")
+for (var i = 1; i <= 10; i++) {
+    document.write(table + "X" + i + "=" + table * i + "<br>")
+}
+
+*/
+
+
+
+//Q6---TEMPERATURE CONVERT F to C
+
+/* var F = prompt("Enter temperature in Fahrenhiet")
+ var C = (F - 32) * 5 / 9
+ document.write("Convert Fahrenhiet to Celcius = " + F + " F" + " to" + C + " C")
+     //---TEMPERATURE CONVERT C to F
+ var C = prompt("Enter temperature in Celcius")
+ var F = (C * 9 / 5) + 32
+ document.write("<br>Convert Celcius to  Fahrenhiet = " + C + " C" + " to" + F + " F")*/
+
+
+
+//Q7-----Shopping
+
+/*var price1 = prompt("Enter price of item1")
+var price2 = prompt("Enter price of item2")
+var quantity1 = prompt("Enter order quantity of item1")
+var quantity2 = prompt("Enter order quantity of item2")
+var shippingcharges = 100
+var cost = price1 * quantity1 + price2 * quantity2 + shippingcharges
+document.write("Price of itme1 is " + price1)
+document.write("<br>Quantity of itme1 is " + quantity1)
+document.write("<br>Price of itme2 is " + price2)
+document.write("<br>Quantity of itme2 is " + quantity2)
+document.write("<br><br>Total cost of your order is " + cost)*/
+
+//Q8---MARKS
+/*var english = prompt("Enter English number")
+var math = prompt("Enter Math number")
+var urdu = prompt("Enter Urdu number")
+var science = prompt("Enter Science number")
+var islamiat = prompt("Enter Islamiat number")
+var physics = prompt("Enter Physics number")
+var chemistry = prompt("Enter Chemistry number")
+var computerscience = prompt("Enter ComputerScience number")
+var socialstudies = prompt("Enter SocialStudies number")
+var practicals = prompt("Enter Practicals number")
+
+var obtainmarks = (+english) + (+math) + (+urdu) + (+science) + (+socialstudies) + (+physics) + (+chemistry) + (+computerscience) + (+practicals) + (+islamiat)
+var totalmarks = 980
+var percentage = (obtainmarks / totalmarks) * 100
+document.write("Total marks: " + totalmarks)
+document.write("<br>Marks Obtained: " + obtainmarks)
+document.write("<br>Percentage: " + percentage + "%")*/
+
+
+
+//Q9-----Currency
+/*  var us1$ = 104.80
+  var saudiRiyal1 = 28
+  var currency = (+10 * us1$) + (+25 * saudiRiyal1)
+  document.write("Total Currency in PKR: " + currency)*/
+
+//Q10-----SEQUENCE VISE MATHEMATICAL eXPRESSION
+/*var MathExpression = 4 + 5 * 10 / 2
+console.log(MathExpression)*/
+
+
+//   Q11-----AGE
+/* var currentyear = prompt("Enter your Current Year")
+ var birthyear = prompt("Enter your Birth Year")
+ var age = currentyear - birthyear
+ document.write("Current Year: " + currentyear)
+ document.write("<br>Birth Year: " + birthyear)
+ document.write("<br>Your age is:  " + age)*/
+
+//Q12-----GEOMETRIZER
+/* var radius = prompt("Enter radius")
+ var circumferrence = 2 * 3.142 * radius
+ document.write("Radius of a circle: " + radius)
+ document.write("<br>The Circumference is: " + circumferrence)
+ var areaOfaCircle = 3.142 * radius * radius
+ document.write("<br>Area of a Circle is : " + areaOfaCircle)*/
+
+//Q13--------LIFETIME SUPPLY CALCULATOR
+
+/* var favsnacks = prompt("Enter favourite Snacks")
+ var currentage = prompt("Enter your current age")
+ var estimateMaxAge = prompt("Enter Estimated Maximum Age")
+ var AmountPerDay = prompt("Enter Amount per Day as a number")
+ var eatTotal = (estimateMaxAge - currentage) * AmountPerDay
+ document.write("Favourite Snack: " + favsnacks)
+ document.write("<br>Current Age: " + currentage)
+ document.write("<br>Estimated Maximum Age: " + estimateMaxAge)
+ document.write("<br>Amount of snacks per day: " + AmountPerDay)
+ document.write("<br>You will need " + eatTotal + " " + favsnacks + " to  last you until the ripe old age of " + estimateMaxAge)*/
+
+//CHAPTER:6-9================================================================
+//Q1----ARITHMETIC 
+/* document.write("Result:")
+ var a = 10
+ document.write("<br>The value of a is: " + a)
+ document.write("<br>........................ ")
+     ++a
+ document.write("<br><br>The value of ++a is: " + a)
+ document.write("<br>Now the value of a is: " + a)
+
+ document.write("<br><br>The value of a++ is: " + a)
+ a++
+ document.write("<br>Now the value of a is: " + a)
+
+ --a
+ document.write("<br><br>The value of --a is: " + a)
+ document.write("<br>Now the value of a is: " + a)
+
+
+ document.write("<br><br>The value of a-- is: " + a)
+ a--
+ document.write("<br>Now the value of a is: " + a)
+ */
+
+
+// Q2---a,b value Execution
+/*  var a = 2,
+      b = 1
+      --a
+  document.write("a is " + a + " b is " + b)
+      --a - --b
+  document.write("<br>a is " + a + " b is " + b)
+      --a - --b + ++b
+  document.write("<br>a is " + a + " b is " + b)
+      --a - --b + ++b + b--
+      document.write("<br>a is " + a + " b is " + b)
+  var result = --a - --b + ++b + b--
+      document.write("<br>result is " + result)
+
+      */
+
+
+//Q3----INPUT USER NAME
+/*  var user = prompt("Enter your Name")
+  document.write("Asslamoaliakum Warhmatullahi Wabarakatuhu " + user)*/
+
+
+//  Q5-----
+// if (table === i) {
+//     var def = 5
+//     for (var i = 1; i <= 10; i++) {
+//         document.write(+def + "x" + i + "=" + def * i + "<br>")
+//     }
+// } else {
+//     var table = prompt("Enter Your any number for display  table")
+
+//     for (var i = 1; i <= 10; i++) {
+//         document.write(+table + "x" + i + "=" + table * i + "<br>")
+
+//     }
+// }
+
+// var usernum = "ANY NUMBER"
+// var defaultnum = 5
+
+
+// var table = prompt(usernum, defaultnum)
+
+
+///Q6----MARKS TABLE
+/* var subject1 = prompt("Enter your subject1 name")
+ var subject2 = prompt("Enter your subject2 name")
+ var subject3 = prompt("Enter your subject3 name")
+ var eachMarks = 100
+ var totalMarks = 300
+ var mark1 = prompt("Enter your subject1 mark")
+ var mark2 = prompt("Enter your subject2 mark")
+ var mark3 = prompt("Enter your subject3 mark")
+ var obtainMarks = (+mark1) + (+mark1) + (+mark3)
+ var percentage = obtainMarks / totalMarks * 100
+ document.write("<b>SubjectTotalMarksObtainedMarksPercentage</b>")
+ document.write("<br>" + subject1 + "  " + eachMarks + "  " + mark1 + "  " + mark1 / eachMarks * 100 + "%")
+ document.write("<br>" + subject2 + "  " + eachMarks + "  " + mark2 + "  " + mark2 / eachMarks * 100 + "%")
+ document.write("<br>" + subject3 + "  " + eachMarks + "  " + mark3 + "  " + mark3 / eachMarks * 100 + "%")
+ document.write("<br> " + totalMarks + "  " + obtainMarks + "   " + percentage + "%     ")*/
+
+//CHAPTER:9-11==============================================================
+//Task:1---
+
+/* var city = prompt("Enter Your city")
+if (city == "karachi") {
+    document.write("Welcome to the City of Light!--" + city)
+} else {
+    document.write("Welcome to City! " + city)
+}*/
+
+//Task:2--------
+/* var gender = prompt("Enter Your Gender")
+ if (gender == "male") {
+     document.write("Good Morning Sir!")
+ } else if (gender == "female") {
+     document.write("Good Morning Maa'm!")
+ }*/
+
+//Task:3-------TABLE-COLOR
+// document.write("<table>")
+
+// var color = prompt("Enter Your color For tarffic Road")
+// document.write("Signal Color <td> Message</td>")
+// document.write("<br>Red             <td> Must Stop<td>")
+// document.write("<br>Yellow           <td>Ready to Move</td>")
+// document.write("<br>Green            <td> Move Now</td>")
+// document.write("</table")
+
+/* var a = prompt("Enter the color");
+        var b = "red";
+        var c = "yellow";
+        var d = "green";
+
+        if (a == b) {
+            document.write("<table ><tr><th>Signalcolor<th><th>Message</th></tr></table>");
+            document.write("<table><tr><td>Red<td>&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;<td>Must stop</td></tr></table>");
+        } else if (a == c) {
+            document.write("<table><tr><th>Signalcolor<th><th>Message</th></tr></table>");
+            document.write("<table><tr><td>Yellow<td>&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;<td>Ready to move</td></tr></table>");
+        } else if (a == d) {
+            document.write("<table><tr><th>Signalcolor<th><th>Message</th></tr></table>");
+            document.write("<table><tr><td>Green<td>&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;<td>Move now</td></tr></table>");
+        } else {
+            document.write("No colors");
+        }
+*/
+
+//Task:4------Fuel Car
+/* var fuel = prompt("Enter the fuel in litres")
+        if (fuel < 0.25) {
+            document.write("Please refill the fuel in car!")
+        } else {
+            document.write("Full fuel in car!")
+        }
+*/
+//Task:5-------------Run script
+
+//a.
+/* var a = 4
+
+if (++a === 5) {
+    alert("given condition for variable a is true")
+} 
+*/
+
+//b.
+/* var b = 82
+ if (b++ === 83) {
+     alert("given condition for variable b is true")
+ }*/
+
+//c.
+/* var c = 12
+ if (c++ === 13) {
+     alert(" condition1 is true")
+ }
+ if (c === 13) {
+     alert(" condition2 is true")
+ }
+ if (++c < 14) {
+     alert(" condition3 is true")
+ }
+
+ if (c === 14) {
+     alert(" condition4 is true")
+ }*/
+
+//d.
+/* var materialCost = 20000
+ var laborCost = 2000
+ var totalCost = materialCost + laborCost
+ if (totalCost === laborCost + materialCost) {
+     alert("The cost equals")
+ }*/
+
+//e.
+
+/* if (false) {
+     alert("False")
+ }
+ if (true) {
+     alert("True")
+ }*/
+
+
+
+
+
+
+////serach waal question HELPDESK------------------------------
+// let arr = ["cakes", "choc", "juice"]
+// document.write(arr.indexOf("juice"))
+// document.write("<br>")
+// document.write(arr.indexOf("choclate"))
+
+// var items = ["biryani", "burger"]
+// var search = prompt("Search value")
+
+// if (items.indexOf(search) !== -1) {
+//     alert("yes " + search + " is avialable")
+
+// } else {
+//     alert("No " + search + " is not  avialable")
+// }
+// document.write("<select>")
+
+// var arr = ["SUMSUNG", "iphone", "Apple"]
+// for (var i = 0; i < arr.length; i++) {
+
+//     document.write("<option>" + arr[i] + "</option>")
+// }
+// document.write("</select>")
+
+
+//ASCII code
+// var a = "A"
+
+// console.log(a.charCodeAt())
+
+//split
+// var word = "ghous"
+// var split = word.split("")
+// console.log(split) //split array mai save aik aik letter ko string ko array mai convert
+
+//split with space
+// var word = "ghous ahmed"
+// var split = word.split(" ")
+// console.log(split)
+
+//reverse array with split
+// var word = "ghous"
+// var split = word.split("").reverse()
+// console.log(split)
+
+//word.split string ko array mai convert .reverse nai array ko reverse kerdia array ko string mai join kerdia
+//split letter join
+// var word = "ghous"
+// var split = word.split("").reverse().join("")
+// console.log(split)
+
+
+// //array splice value add marzi
+// var arr = ["farii", "walii", "sadiii"]
+// var updated = arr.splice(0, 0, "Amna")
